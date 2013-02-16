3241084bcb30e3d66b65fd5837ace0c082fe5b3b
Laszlo David <laszlo.david@gmail.com>
2013-02-16 01:23:12 +0100

    upto date now

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3206,7 +3206,19 @@ public final class Settings {
           */
          public static final String KEY_ASSIST_ACTION = "key_assist_action";
 
+<<<<<<< HEAD
           
+=======
+         /**
+          * Swap volume buttons when the screen is rotated
+          * 0 - Disabled
+          * 1 - Enabled (screen is rotated by 90 or 180 degrees: phone, hybrid)
+          * 2 - Enabled (screen is rotated by 180 or 270 degrees: tablet)
+          * @hide
+          */
+         public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
+
+>>>>>>> 2c68448... Swap volume buttons when the screen is rotated (1/2)
         /**
          * Weather to minimize lockscreen challenge on screen turned on
          * @hide
