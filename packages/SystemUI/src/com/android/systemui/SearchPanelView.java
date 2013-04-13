e59d6d4a71aca6727c4b2c6821d7c8421efeb14d
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-04-13 21:21:16 +0700

    SystemUI: fix memory leaks
    ContentObservers are being registered without being unregistered,
    causing old objects not to be garbage collected. This causes memory
    leak if the classes are destroyed and recreated throughout the
    device's uptime.
    
    SearchPanelView is recreated every time the screen is turned on.
    StatusBarIconView is created every time a notification is triggered.
    
    JIRA: CYAN-650
    Change-Id: Iabd3221fc2b0c848396af1790fd09424be602e7e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -104,6 +104,7 @@ public class SearchPanelView extends FrameLayout implements
     private final Context mContext;
     private BaseStatusBar mBar;
     private StatusBarTouchProxy mStatusBarTouchProxy;
+    private SettingsObserver mObserver;
 
     private boolean mShowing;
     private View mSearchTargetsContainer;
@@ -158,6 +159,8 @@ public class SearchPanelView extends FrameLayout implements
             switch (m.what) {
             }
         }
+        
+        SettingsObserver observer = new SettingsObserver(new Handler());
     }
 
     private H mHandler = new H();
@@ -239,9 +242,6 @@ public class SearchPanelView extends FrameLayout implements
         // TODO: fetch views
         mGlowPadView = (GlowPadView) findViewById(R.id.glow_pad_view);
         mGlowPadView.setOnTriggerListener(mGlowPadViewListener);
-
-        updateSettings();
-        setDrawables();
     }
 
     private void setDrawables() {
@@ -492,6 +492,21 @@ public class SearchPanelView extends FrameLayout implements
         return true;
     }
 
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        mObserver.observe();
+        updateSettings();
+        setDrawables();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mObserver.unobserve();
+    }
+
     /**
      * Whether the panel is showing, or, if it's animating, whether it will be
      * when the animation is done.
@@ -574,6 +589,10 @@ public class SearchPanelView extends FrameLayout implements
                     Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING_LONG[i]), false, this);
             }
 
+        }
+
+        void unobserve() {
+            mContext.getContentResolver().unregisterContentObserver(this);
         }
 
         @Override
