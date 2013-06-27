777d6dd1c44bf7d2aa16fcfcd1ac665f59dcb2d9
Steve Kondik <shade@chemlab.org>
2013-06-27 00:07:34 -0700

    am: Fix the privacy guard notification
     * The privacy guard notification was not showing everytime. Fix the
       logic and handle it in the right place so it hits both the create and
       resume startup path.
    
    Change-Id: I80c88ffb0fcb4ed3ea64ceb228bb436975278ecc
    
    Conflicts:
    	services/java/com/android/server/am/ActivityStack.java

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -287,6 +287,11 @@ final class ActivityStack {
      */
     boolean mDismissKeyguardOnNextActivity = false;
 
+    /**
+     * Is the privacy guard currently enabled?
+     */
+    String mPrivacyGuardPackageName = null;
+
     int mThumbnailWidth = -1;
     int mThumbnailHeight = -1;
 
@@ -1256,6 +1261,7 @@ final class ActivityStack {
         } else {
             next.cpuTimeAtResume = 0; // Couldn't get the cpu time of process
         }
+        updatePrivacyGuardNotificationLocked(next);
     }
 
     /**
@@ -1755,7 +1761,7 @@ final class ActivityStack {
                 next.app.pendingUiClean = true;
                 next.app.thread.scheduleResumeActivity(next.appToken,
                         mService.isNextTransitionForward());
-                
+
                 checkReadyForSleepLocked();
 
             } catch (Exception e) {
@@ -1816,11 +1822,8 @@ final class ActivityStack {
             startSpecificActivityLocked(next, true, true);
         }
 
-        handlePrivacyGuardNotification(prev, next);
-
         return true;
     }
-
 
     public void pf(String tag, ActivityRecord r) {
         if (r == null) return;
@@ -1854,35 +1857,31 @@ final class ActivityStack {
         }
     }
 
-    private final void handlePrivacyGuardNotification(ActivityRecord prev, ActivityRecord next) {
-        boolean curPrivacy = false;
-        boolean prevPrivacy = false;
+    private final void updatePrivacyGuardNotificationLocked(ActivityRecord next) {
 
-        if (next != null) {
-            try {
-                curPrivacy = AppGlobals.getPackageManager().getPrivacyGuardSetting(
-                        next.packageName, next.userId);
-            } catch (RemoteException e) {
-                // nothing
-            }
+        if (mPrivacyGuardPackageName != null && mPrivacyGuardPackageName.equals(next.packageName)) {
+            return;
         }
-        if (prev != null) {
-            try {
-                prevPrivacy = AppGlobals.getPackageManager().getPrivacyGuardSetting(
-                        prev.packageName, prev.userId);
-            } catch (RemoteException e) {
-                // nothing
-            }
+
+        boolean privacy = false;
+
+        try {
+            privacy = AppGlobals.getPackageManager().getPrivacyGuardSetting(
+                    next.packageName, next.userId);
+        } catch (RemoteException e) {
+            // nothing
         }
-        if (prevPrivacy && !curPrivacy) {
+
+        if (mPrivacyGuardPackageName != null && !privacy) {
             Message msg = mService.mHandler.obtainMessage(
-                    ActivityManagerService.CANCEL_PRIVACY_NOTIFICATION_MSG, prev.userId);
+                    ActivityManagerService.CANCEL_PRIVACY_NOTIFICATION_MSG, next.userId);
             msg.sendToTarget();
-        } else if ((!prevPrivacy && curPrivacy) ||
-                (prevPrivacy && curPrivacy && !next.packageName.equals(prev.packageName))) {
+            mPrivacyGuardPackageName = null;
+        } else if (privacy) {
             Message msg = mService.mHandler.obtainMessage(
                     ActivityManagerService.POST_PRIVACY_NOTIFICATION_MSG, next);
             msg.sendToTarget();
+            mPrivacyGuardPackageName = next.packageName;
         }
     }
 
