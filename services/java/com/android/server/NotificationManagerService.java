1ca9166875c16f505110efed3557792c5f1df5f7
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-07-09 17:14:34 +0100

    Revert "Add permission that allows cancelling notifications from other apps."
    This reverts commit bd9cf335265c85f8f9f25f063742fcaacf194ea3.

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1583,10 +1583,6 @@ public class NotificationManagerService extends INotificationManager.Stub
     }
 
     void checkCallerIsSystemOrSameApp(String pkg) {
-        if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.CANCEL_NOTIFICATIONS)
-                == PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
         int uid = Binder.getCallingUid();
         if (UserHandle.getAppId(uid) == Process.SYSTEM_UID || uid == 0) {
             return;
